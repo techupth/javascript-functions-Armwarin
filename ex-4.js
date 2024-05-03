1639959a8e67ce42f37669883ec0c3f36e528163
Armwarin <warin35413@gmail.com>
2024-05-03 16:45:58 +0700

    bp3-arm

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,18 @@
 // Start coding here
-let calculator = {};
+ let calculator = {
+    add: function(num1, num2) {
+      return num1 + num2;
+    },
+    subtract: function(num1, num2) {
+      return num1 - num2;
+    },
+    multiply: function(num1, num2) {
+      return num1 * num2;
+    },
+    divide: function(num1, num2) {
+      if (num2 === 0) {
+        return "Error: Divide by zero";
+      }
+      return num1 / num2;
+    }
+  }; 
